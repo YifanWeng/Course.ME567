c6ed42b127a174bf358adf261cb5097a81d2ed6f
YifanWeng <frankweng940412@hotmail.com>
2017-04-10 20:24:34 -0400

    Update TCP_IP.m

--- TCP_IP.m
+++ TCP_IP.m
@@ -5,18 +5,26 @@ clc
 clear all
 close all
 
+flag = 1;
 
-%% sender
-t = tcpip('localhost', 30000, 'NetworkRole', 'client');
-fopen(t);
-fwrite(t, data);
-data = sin(1:64);
-plot(data);
+if flag
+    %% sender
+    data = sin(1:64);
+    plot(data);
+    %echotcpip('on',30000);
+    t = tcpip('localhost', 30000, 'NetworkRole', 'client');
+    fopen(t);
+    fwrite(t, data);
+    data = sin(1:64);
+    plot(data);
 
 
+else
+    %% receiver
+    echotcpip('on',30000);
+    t = tcpip('0.0.0.0', 30000, 'NetworkRole', 'server');
+    fopen(t);
+    data = fread(t, t.BytesAvailable);
+    plot(data);
 
-%% receiver
-t = tcpip('0.0.0.0', 30000, 'NetworkRole', 'server');
-fopen(t);
-data = fread(t, t.BytesAvailable);
-plot(data);
+end
